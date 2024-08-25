964f7d0d4f602e6b26aab83eec7be0df7e895ce8
NasiRames <panjisetyokurniawan@permataazzurisehat.com>
2024-08-25 19:36:26 +0700

    update setting app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,18 @@ import { PORT } from './uhuuy.json'
 import { indexRouter } from './routes'
 import { morganNotes } from './config/morgan'
 import { errorHandler } from './exception/exception.global'
+import { helmetHandler } from './config/helmet'
+import { compressionHandler } from './config/compression'
 
 const app: Express = express()
 
 pgClient.connect()
 
 app.set('trust proxy', true)
+app.use(helmetHandler())
 app.use(json({ type: ['application/json', 'application/csp-report', 'application/reports+json'] }))
 app.use(morganNotes())
+app.use(compressionHandler())
 app.use(indexRouter())
 app.use(errorHandler())
 
